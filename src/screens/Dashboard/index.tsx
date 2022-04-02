278ea2e08393660403b3686fda87762fa16a6eea
unknown <lucastheborges@gmail.com>
2022-04-02 17:38:16 -0300

    Criação de uma scrollView para rolagem dos cards

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -10,6 +10,7 @@ import {
     UserGreeting,
     UserName,
     Icon,
+    HighlightCards
 } from "./styles";
 
 export function Dashboard() {
@@ -30,7 +31,11 @@ export function Dashboard() {
                 </UserWrapper>
             </Header>
 
-            <HighlightCard />
+            <HighlightCards>
+                <HighlightCard />
+                <HighlightCard />
+                <HighlightCard />
+            </HighlightCards>
 
         </Container >
     )
