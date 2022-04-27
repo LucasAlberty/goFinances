bbb80500463c51c4226e533c51c3a5a4436c8cc5
unknown <lucastheborges@gmail.com>
2022-04-26 21:03:52 -0300

    Remover dados do AsyncStorage

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { 
+import {
     Keyboard,
     Modal,
     TouchableWithoutFeedback,
@@ -28,7 +28,7 @@ import {
     TransactionTypes
 } from "./styles";
 
-interface FormData{
+interface FormData {
     name: string;
     amount: string;
 
@@ -36,13 +36,13 @@ interface FormData{
 
 const scheme = Yup.object().shape({
     name: Yup
-    .string()
-    .required('Nome é obrigatório'),
+        .string()
+        .required('Nome é obrigatório'),
     amount: Yup
-    .number()
-    .typeError('Informe um valor númerico')
-    .positive('O valor não pode ser negativo')
-    .required('O valor é obrigatório')
+        .number()
+        .typeError('Informe um valor númerico')
+        .positive('O valor não pode ser negativo')
+        .required('O valor é obrigatório')
 })
 
 export function Register() {
@@ -78,23 +78,31 @@ export function Register() {
 
     async function handleRegister(form: FormData) {
 
-        if(!transactionType){
+        if (!transactionType) {
             return Alert.alert('Selecione o tipo da transação!')
         }
 
-        if(category.key === 'category'){
+        if (category.key === 'category') {
             return Alert.alert('Selecione uma categoria!')
         }
 
-        const data = {
+        const newTransaction = {
             name: form.name,
             amount: form.amount,
             transactionType,
             category: category.key
-        } 
+        }
 
         try {
-            await AsyncStorage.setItem(dataKey, JSON.stringify(data));
+            const data = await AsyncStorage.getItem(dataKey);
+            const currentData = data ? JSON.parse(data) : [];
+
+            const dataFormated = [
+                ...currentData,
+                newTransaction
+            ];
+
+            await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormated));
         } catch (error) {
             console.log(error)
             Alert.alert('Não foi possivel salvar.')
@@ -102,18 +110,23 @@ export function Register() {
     }
 
     useEffect(() => {
-        async function loadData(){
-           const data = await AsyncStorage.getItem(dataKey);
-           console.log(JSON.parse(data!));
+        async function loadData() {
+            const data = await AsyncStorage.getItem(dataKey);
+            console.log(JSON.parse(data!));
         };
 
         loadData();
+
+        /*      async function removeAll() {
+                 await AsyncStorage.removeItem(dataKey);
+             }
+             removeAll(); */
     }, [])
 
     return (
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
             <Container>
-                
+
                 <Header>
                     <Title>Cadastro</Title>
                 </Header>
@@ -123,7 +136,7 @@ export function Register() {
                     <Fields>
                         <InputForm
                             name='name'
-                            control={ control }
+                            control={control}
                             placeholder="Nome"
                             autoCapitalize="sentences"
                             autoCorrect={false}
@@ -131,7 +144,7 @@ export function Register() {
                         />
                         <InputForm
                             name='amount'
-                            control={ control}
+                            control={control}
                             placeholder="Preço"
                             keyboardType="numeric"
                             error={errors.amount && errors.amount.message}
